740abaeb52e815631bb05ddf318b0fb9d0e5ccc7
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-02-10 22:49:50 +0200

    add project

---
+++ Chris28/Hello/Hello/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Volodymyr Ostapyshyn on 08.02.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TheSwiftDevs/MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var name1 = "Oleg"
+
+let name2 = "Ivan"
+//name2 = "Orest"
+print(name1, name2 + " !")
+print(name1 + name2)
+
+//var code = "123" + 456 + "789" but works in Kotlin
+
