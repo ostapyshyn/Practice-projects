fd1a67efc28f696b9891bf0ff736d342be840818
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-02-17 23:35:18 +0200

    finish project

---
+++ Chris28/ExerciseLesson4/ExerciseLesson4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExerciseLesson4
+//
+//  Created by Volodymyr Ostapyshyn on 17.02.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hello/WhichNumberIsBigger/WhichNumberIsBigger/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  WhichNumberIsBigger
+//
+//  Created by Volodymyr Ostapyshyn on 17.02.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var number1: UITextField!
+    @IBOutlet var number2: UITextField!
+    @IBOutlet var outputLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        if Int(number1.text!)! > Int(number2.text!)! {
+            outputLabel.text = "\(number1.text!) is greater than \(number2.text!)"
+        } else if Int(number1.text!)! < Int(number2.text!)! {
+            outputLabel.text = "\(number1.text!) is smaller than \(number2.text!)"
+        } else {
+            outputLabel.text = "\(number1.text!) is equal to \(number2.text!)"
+        }
+    }
+}
+
