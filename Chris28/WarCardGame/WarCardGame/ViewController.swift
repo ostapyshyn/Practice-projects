5ed5945b4d0333a79c0d38632fbfd9ac7ee0bc80
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-02-18 23:43:38 +0200

    finish war card game

--- Chris28/WarCardGame/WarCardGame/ViewController.swift
+++ Chris28/WarCardGame/WarCardGame/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var leftImageView: UIImageView!
+    
+    @IBOutlet var rightImageView: UIImageView!
+    
+    @IBOutlet var leftScoreLabel: UILabel!
+    
+    @IBOutlet var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func dealTapped(_ sender: UIButton) {
+        let leftNumber = Int.random(in: 2...14)
+        
+        let rightNumber = Int.random(in: 2...14)
 
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber {
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+            
+        } else if rightNumber > leftNumber {
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        
+    }
+    
 }
 
