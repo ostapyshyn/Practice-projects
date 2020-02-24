6c1446ae5ee271b58bddab58d7092a6b54c3f94d
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-02-24 23:29:16 +0200

    add delegates Match App

--- Chris28/Match App/Match App/ViewController.swift
+++ Chris28/Match App/Match App/ViewController.swift
@@ -8,17 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    
     
     var model = CardModel()
     var cardArray = [Card]()
     
-
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         cardArray = model.getCards()
+        
+        
     
         
     }
