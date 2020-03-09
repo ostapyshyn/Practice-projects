b99d98143e3103746689297ec74f572c3d19f304
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-03-09 23:32:08 +0200

    add new project

---
+++ AppCodaB2/AppCodaB2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppCodaB2
+//
+//  Created by Volodymyr Ostapyshyn on 09.03.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle:UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+}
+
---
+++ The Night Porter/The Night Porter/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  The Night Porter
+//
+//  Created by Volodymyr Ostapyshyn on 07.03.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackground(_ sender: UIButton) {
+        view.backgroundColor = UIColor.darkGray
+        
+    }
+    
+}
+
