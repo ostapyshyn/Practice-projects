0760ae79b674f5da1bcea8a98b2bd5343c5efe05
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-04-08 22:28:23 +0300

    finish ui

---
+++ SwiftBook/WeeklyFinder/WeeklyFinder/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Volodymyr Ostapyshyn on 08.04.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var dateTF: UITextField!
+    @IBOutlet var monthTF: UITextField!
+    @IBOutlet var yearTF: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func findDayPressed(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
