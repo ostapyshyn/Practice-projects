8015385159b99e6c6e843deca42b0e93397bb802
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-04-20 22:28:43 +0300

    add custom button

---
+++ CardProgram/CardProgram/CButton.swift
@@ -0,0 +1,40 @@
+//
+//  CButton.swift
+//  CardProgram
+//
+//  Created by Volodymyr Ostapyshyn on 20.04.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class CButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init (backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+        
+    }
+    
+    
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+    }
+}
---
+++ CardProgram/CardProgram/CardSelectionVC.swift
@@ -0,0 +1,18 @@
+//
+//  CardSelectionVC.swift
+//  CardProgram
+//
+//  Created by Volodymyr Ostapyshyn on 20.04.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+    }
+}
