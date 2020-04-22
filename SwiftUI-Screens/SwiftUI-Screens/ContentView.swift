1e3404ddffc7bc64ebc6711ba0a9a3a504ba329a
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-04-22 19:36:19 +0300

    finish SwiftUI Screen project

--- SwiftUI-Screens/SwiftUI-Screens/ContentView.swift
+++ SwiftUI-Screens/SwiftUI-Screens/ContentView.swift
@@ -8,14 +8,60 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
+    
+    var videos: [Video] = VideoList.topTwelve
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(videos) { video in
+                Image(video.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 90.0)
+                    .cornerRadius(8)
+                
+                Spacer().frame(width: 16)
+                
+                VStack(alignment: .leading) {
+                    Text(video.title)
+                        .fontWeight(.semibold)
+                        .lineLimit(2)
+                        .minimumScaleFactor(0.75)
+                    Spacer().frame(height: 5)
+                    Text(video.uploadDate)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    
+                }
+            }.navigationBarTitle(Text("Sean's Videos"))
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
