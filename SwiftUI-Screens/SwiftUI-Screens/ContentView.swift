869bfe17f787dc5099e90da783b27d45269da2de
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-04-22 14:47:17 +0300

    start SwiftUI basic Screen project

---
+++ SwiftUI-Screens/SwiftUI-Screens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Screens
+//
+//  Created by Volodymyr Ostapyshyn on 22.04.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
