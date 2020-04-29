1c6c23282b27dd46ea77319e7cd3ab246a6bad22
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-04-29 22:30:13 +0300

    start T-Converter project

---
+++ SwiftBook/TConverter/TConverter/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Volodymyr Ostapyshyn on 29.04.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var celciusLabel: UILabel!
+    @IBOutlet var fahrenheitLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func slderChanged(_ sender: UISlider) {
+        
+    }
+    
+
+}
+
