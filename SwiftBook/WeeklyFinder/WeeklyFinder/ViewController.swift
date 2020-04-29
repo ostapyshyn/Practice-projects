3adcefc63cdd3f06d340a951c0ec1c9ec39921fb
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-04-29 19:33:41 +0300

    finish WeekDay Finder project

--- SwiftBook/WeeklyFinder/WeeklyFinder/ViewController.swift
+++ SwiftBook/WeeklyFinder/WeeklyFinder/ViewController.swift
@@ -23,9 +23,28 @@ class ViewController: UIViewController {
     
     
     @IBAction func findDayPressed(_ sender: UIButton) {
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "uk_UA")
+        dateFormatter.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let weekday = dateFormatter.string(from: date).capitalized
+        
+        resultLabel.text = weekday
         
     }
     
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
