1079a026976766d657d6015012251153ac83e954
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-05-02 14:47:49 +0300

    start WorldTrotter BRD

---
+++ BigNerdRanch/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Volodymyr Ostapyshyn on 01.05.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
+
