e310baebc4eeafe7be3c4aa699f53519401e3d2b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-07-03 22:26:17 +0300

    Finish chapter 7

---
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Volodymyr Ostapyshyn on 03.07.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
