44ccdde674c638c712cb8ca2284edc4975d73a87
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-07-13 23:08:17 +0300

    Add new struct

---
+++ FoodPinDetailStarter/FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by Volodymyr Ostapyshyn on 13.07.2020.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+
+    @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel! {
+        didSet {
+            nameLabel.numberOfLines = 0
+            
+        }
+    }
+    @IBOutlet var typeLabel: UILabel! {
+        didSet {
+            typeLabel.layer.cornerRadius = 5.0
+            typeLabel.layer.masksToBounds = true
+            
+        }
+    }
+    @IBOutlet var heartImageView: UIImageView!
+
+
+}
---
+++ StartUI/StartUI/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  StartUI
+//
+//  Created by Volodymyr Ostapyshyn on 12.07.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView {
+            Text("I Am Rich")
+            Text("I Am Rich")
+            Text("I Am Rich")
+            Text("I Am Rich")
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
