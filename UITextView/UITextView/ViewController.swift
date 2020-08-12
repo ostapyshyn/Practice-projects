b1dc8c8762b2127d5ca4a1e3edbb9a30df7c1405
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-08-12 22:32:07 +0300

    Add TextView

--- UITextView/UITextView/ViewController.swift
+++ UITextView/UITextView/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myTextView = UITextView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            // Do any additional setup after loading the view.
+        
+        myTextView = UITextView(frame: view.bounds)
+        myTextView.text = "SomeText"
+        myTextView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
+        myTextView.font = UIFont.systemFont(ofSize: 17)
+        myTextView.backgroundColor = .gray
+        view.addSubview(myTextView)
     }
 
 
