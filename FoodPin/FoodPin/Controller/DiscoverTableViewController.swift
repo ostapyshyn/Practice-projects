e61063a3b6377d3ab1b48ec79bf21e938d2a2b20
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-08-25 21:28:34 +0300

    Add CloudKit

--- FoodPin/FoodPin/Controller/DiscoverTableViewController.swift
+++ FoodPin/FoodPin/Controller/DiscoverTableViewController.swift
@@ -12,47 +12,91 @@ import CloudKit
 class DiscoverTableViewController: UITableViewController {
 
     var restaurants: [CKRecord] = []
+    var spinner = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        spinner.style = .medium
+        spinner.hidesWhenStopped = true
+        view.addSubview(spinner)
+        
+        // Define layout constraints for the spinner
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([ spinner.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant:150.0),
+                                      spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
+        
+        
+        // Activate the spinner
+        spinner.startAnimating()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Configure navigation bar appearance
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
+        navigationController?.navigationBar.largeTitleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont ]
+        }
+        fetchRecordsFromCloud()
+    }
+    
+    func fetchRecordsFromCloud() {
+        // Fetch data using Convenience API
+        let cloudContainer = CKContainer.default()
+        let publicDatabase = cloudContainer.publicCloudDatabase
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Restaurant", predicate: predicate)
+        publicDatabase.perform(query, inZoneWith: nil, completionHandler: {
+            (results, error) -> Void in
+            if let error = error { print(error)
+                return
+            }
+            if let results = results {
+                print("Completed the download of Restaurant data")
+                self.restaurants = results
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+            
+        })
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return restaurants.count
+        
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DiscoverCell" , for: indexPath)
+        
         // Configure the cell...
-
+        let restaurant = restaurants[indexPath.row]
+        cell.textLabel?.text = restaurant.object(forKey: "name") as? String
+        
+        if let image = restaurant.object(forKey: "image"), let imageAsset = image as? CKAsset {
+            
+            if let imageData = try? Data.init(contentsOf: imageAsset.fileURL!) { cell.imageView?.image = UIImage(data: imageData)
+            }
+        }
         return cell
     }
-    */
+   
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+    
 
+    
+    
     /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
