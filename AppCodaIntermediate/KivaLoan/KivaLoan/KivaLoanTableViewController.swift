8cdfb156cb2534c3c91b079cf1c5b07888a9ac86
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-09-12 21:30:25 +0300

    Add KivaLoan Project

---
+++ AppCodaIntermediate/KivaLoan/KivaLoan/KivaLoanTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  KivaLoanTableViewController.swift
+//  KivaLoan
+//
+//  Created by Simon Ng on 4/10/2016.
+//  Updated by Simon Ng on 6/12/2017.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class KivaLoanTableViewController: UITableViewController {
+    
+    private let kivaLoanURL = "https://api.kivaws.org/v1/loans/newest.json"
+    private var loans = [Loan]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.estimatedRowHeight = 92.0
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! KivaLoanTableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+
+}
---
+++ AppCodaIntermediate/KivaLoan/KivaLoan/Loan.swift
@@ -0,0 +1,18 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Volodymyr Ostapyshyn on 12.09.2020.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+struct Loan {
+    
+    var name: String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+    
+}
