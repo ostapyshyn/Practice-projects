5c4493c31c76b109b933c6a807ac7523cb7cf53e
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-09-14 08:35:21 +0300

    Add Simple JSON

---
+++ AppCodaIntermediate/SimpleJSON.playground/Contents.swift
@@ -0,0 +1,100 @@
+import UIKit
+
+ // simple JSON
+
+let json = """
+{
+"name": "John Davis",
+"country": "Peru",
+"use": "to buy a new collection of clothes to stock her shop before the holidays." ,
+"loan_amount": 150
+}
+"""
+
+
+
+//struct Loan: Codable {
+//
+//    var name: String
+//    var country: String
+//    var use: String
+//    var amount: Int
+//
+//    enum CodingKeys: String, CodingKey {
+//        case name
+//        case country
+//        case use
+//        case amount = "loan_amount"
+//    }
+//
+//}
+
+
+
+//let decoder = JSONDecoder()
+//
+//if let jsonData = json.data(using: .utf8) {
+//    do {
+//        let loan = try decoder.decode(Loan.self, from: jsonData)
+//        print(loan)
+//    } catch {
+//        print(error)
+//    }
+//
+//}
+
+
+// nested JSON
+
+let json2 = """
+{
+"name": "John Davis",
+"location": {
+"country": "Peru",
+},
+"use": "to buy a new collection of clothes to stock her shop before the holidays." ,
+"loan_amount": 150
+}
+"""
+
+
+struct Loan: Codable {
+    var name: String
+    var country: String
+    var use: String
+    var amount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case country = "location"
+        case use
+        case amount = "loan_amount"
+    }
+    
+    enum LocationKeys: String, CodingKey {
+        case country
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: .name)
+        let location = try values.nestedContainer(keyedBy: LocationKeys.self, forKey: .country)
+        country = try location.decode(String.self, forKey: .country)
+        use = try values.decode(String.self, forKey: .use)
+        amount = try values.decode(Int.self, forKey: .amount)
+        
+    }
+        
+}
+
+let decoder = JSONDecoder()
+
+if let jsonData = json2.data(using: .utf8) {
+    do {
+        let loan = try decoder.decode(Loan.self, from: jsonData)
+        print(loan)
+    } catch {
+        print(error)
+    }
+
+}
